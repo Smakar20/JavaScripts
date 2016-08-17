86230c84a014b979145b8d4cfad2e5d654bfba7a
Sohini Makar <sohini.makar@gmail.com>
2016-08-17 16:02:11 -0700

    Matrix
    Matrix algorithms

---
+++ Algorithms/AbsDiffMatrixSum.js
@@ -0,0 +1,38 @@
+//Find Absolute diagonal sum difference of a square matrix.
+function MartixAbsSum(matrix)
+{
+	this.matrix = matrix;
+	this.checkMatrix = function(){
+		var i = 0;
+		while(i < this.matrix.length)
+		{
+			if(this.matrix.length !== this.matrix[i].length)
+			{
+				return false;
+			}
+			i += 1;
+		}
+		return true;
+	}
+
+	this.matrixSum = function(){
+		if(!this.checkMatrix())
+		{
+			return "Can not calculate absolute sum";
+		}
+		this.diagonalSum1 = 0;
+		this.diagonalSum2 = 0;
+		for(var i=0;i<this.matrix.length;i++)
+		{
+			this.diagonalSum1 += matrix[i][i];
+			this.diagonalSum2 += matrix[matrix.length-1-i][i];
+		}
+		return Math.abs(this.diagonalSum1-this.diagonalSum2);
+	}
+}
+
+var matrix=[[5,2,3],
+			[3,4,5],
+			[5,6,7]];
+var calMatrixSum = new MartixAbsSum(matrix);
+console.log(calMatrixSum.matrixSum());
---
+++ Algorithms/HourGlassSum.js
@@ -0,0 +1,55 @@
+//Calculate the hourglass sum for every hourglass in an 2D array, then print the maximum hourglass sum.
+/*
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+
+-------
+a b c
+  d
+e f g
+
+*/
+function HourGlass(matrix)
+{
+	this.matrix = matrix;
+	this.checkMatrix = function(){
+		var i = 0;
+		while(i < this.matrix.length)
+		{
+			if(this.matrix.length !== this.matrix[i].length)
+			{
+				return false;
+			}
+			i += 1;
+		}
+		return true;
+	}
+	this.greaterHourGlass = function(){
+		if(!this.checkMatrix())
+		return "It's not a square matrix";
+		this.greaterHourGlassSum = 0;
+		var hourGlassSum = 0;
+		for(var i = 0; i< this.matrix.length-2; i++)
+		{
+			for(var j = 0; j< this.matrix.length-2; j ++)
+			{
+				hourGlassSum = matrix[i][j]+matrix[i][j+1]+matrix[i][j+2]+matrix[i+1][j+1]+matrix[i+2][j]+matrix[i+2][j+1]+matrix[i+2][j+2];
+				if(this.greaterHourGlassSum < hourGlassSum)
+				{
+					this.greaterHourGlassSum = hourGlassSum;
+				}
+				
+			}
+		}
+	return this.greaterHourGlassSum;
+	}
+}
+
+// ------- test ----------//
+var matrix = [[1,1,1,0,0,0],[0,1,0,0,0,0],[1,1,1,0,0,0],[0,0,2,4,4,0],[0,0,0,2,0,0],[0,0,1,2,4,0]];
+var hourGlass = new HourGlass(matrix);
+console.log(hourGlass.greaterHourGlass());
