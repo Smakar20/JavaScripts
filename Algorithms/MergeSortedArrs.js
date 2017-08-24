789adee25d57abc9a327bd576f777f815c69171c
Sohini Makar <sohini.makar@gmail.com>
2017-08-24 15:38:04 -0700

    Update MergeSortedArrs.js

--- Algorithms/MergeSortedArrs.js
+++ Algorithms/MergeSortedArrs.js
@@ -1,22 +1,25 @@
 //Merging 2 sorted arrays.
+'use strict'
 function sorting(arr1,arr2)
 {
+  var position = 0
 	arr1.forEach(function(ele){
-		for(i=0;i<arr2.length;i++)
+		for(var i=position; i<arr2.length; i++)
 		{
 			if(arr2[i]>=ele)
 			{
-				arr2.splice(i,0,ele);
-				break;
+				arr2.splice(i,0,ele)
+				position = i + 1
+				break
 			}
 			else if(i == arr2.length-1)
 			{
-				arr2.push(ele);
-				break;
+				arr2.push(ele)
+				break
 			}
 		}
-	});
-	return arr2;
+	})
+	return arr2
 }
 
 sorting([1,2,4,6,8,9,10],[3,5,7]);
