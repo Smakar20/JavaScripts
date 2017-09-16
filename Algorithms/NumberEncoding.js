b4b82c187c7582142540062cdbb55acc1b5a0e14
Sohini Makar <sohini.makar@gmail.com>
2017-09-16 10:59:14 -0700

    Create NumberEncoding.js

---
+++ Algorithms/NumberEncoding.js
@@ -0,0 +1,18 @@
+/*Have the function NumberEncoding(str) take the str parameter and encode the message according to the following rule: encode every 
+letter into its corresponding numbered position in the alphabet. Symbols and spaces will also be used in the input. For example: if str 
+is "af5c a#!" then your program should return 1653 1#!.*/
+
+function NumberEncoding(str) { 
+    var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    var str1 = str.toLowerCase()
+    var output = ""
+    for(var i in str1){
+        if(str1[i].match(/^[a-z]+$/)){
+            output += alphabet.indexOf(str1[i]) + 1
+        }
+        else{
+            output += str1[i]
+        }
+    }
+    return output
+}
