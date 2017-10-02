5ec0903df537fe20fcadb1c5696844076ae7997a
Sohini Makar <sohini.makar@gmail.com>
2017-10-02 16:37:43 -0700

    Update BasicRomanNumerals.js

--- Algorithms/BasicRomanNumerals.js
+++ Algorithms/BasicRomanNumerals.js
@@ -1,6 +1,11 @@
-/*Have the function BasicRomanNumerals(str) read str which will be a string of Roman numerals. The numerals being used are: I for 1, V for 5, X for 10, L for 50, C for 100, D for 500 and M for 1000. In Roman numerals, to create a number like 11 you simply add a 1 after the 10, so you get XI. But to create a number like 19, you use the subtraction notation which is to add an I before an X or V (or add an X before an L or C). So 19 in Roman numerals is XIX. 
+/*Have the function BasicRomanNumerals(str) read str which will be a string of Roman numerals. The numerals being used are: 
+I for 1, V for 5, X for 10, L for 50, C for 100, D for 500 and M for 1000. In Roman numerals, to create a number like 11 you 
+simply add a 1 after the 10, so you get XI. But to create a number like 19, you use the subtraction notation which is to add 
+an I before an X or V (or add an X before an L or C). So 19 in Roman numerals is XIX. 
 
-The goal of your program is to return the decimal equivalent of the Roman numeral given. For example: if str is "XXIV" your program should return 24 */
+The goal of your program is to return the decimal equivalent of the Roman numeral given. For example: if str is "XXIV" your 
+program should return 24 */
+
 (function BasicRomanNumerals(str) {
     var sum = 0
     var obj = {'I': 1,
