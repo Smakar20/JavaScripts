57ec41d83e5f62bb76085f65572a2e3ca6909bb6
Sohini Makar <sohini.makar@gmail.com>
2017-10-19 13:53:46 -0700

    Update OtherProducts.js

--- Algorithms/OtherProducts.js
+++ Algorithms/OtherProducts.js
@@ -14,3 +14,32 @@ least 1 element of only positive integers.*/
     }
     return prodArr.join("-")
 }([3,1,2,6])) // '12-36-18-6'
+
+//other method:
+(function OtherProducts(intArray) {
+     if (intArray.length < 2) {
+        throw new Error('Getting the product of numbers at other indices requires at least 2 numbers')
+    }
+
+    var productsOfAllIntsExceptAtIndex = []
+
+    // for each integer, we find the product of all the integers
+    // before it, storing the total product so far each time
+    var productSoFar = 1
+    for (var i = 0; i < intArray.length; i++) {
+        productsOfAllIntsExceptAtIndex[i] = productSoFar
+        productSoFar *= intArray[i]
+    }
+
+    // for each integer, we find the product of all the integers
+    // after it. since each index in products already has the
+    // product of all the integers before it, now we're storing
+    // the total product of all other integers
+    productSoFar = 1
+    for (var j = intArray.length - 1; j >= 0; j--) {
+        productsOfAllIntsExceptAtIndex[j] *= productSoFar
+        productSoFar *= intArray[j]
+    }
+
+    return productsOfAllIntsExceptAtIndex.join("-")
+})([3,1,2,6]) // '12-36-18-6'
