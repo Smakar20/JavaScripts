96ef8db51d6db8e176cfff4d3e993aa2eee59be1
Sohini Makar <sohini.makar@gmail.com>
2018-01-09 11:07:20 -0800

    determine if the input string is valid

--- Algorithms/bracketMatcher.js
+++ Algorithms/bracketMatcher.js
@@ -1,3 +1,25 @@
+/*Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.*/
+
+//method 1
+
+(function isValid(s) {
+    var bracArr = []
+    for(var i = 0; i < s.length; i++){
+        if(bracArr.length ==0 && (s[i] == ')' || s[i] == '}' || s[i] == ']')) return false 
+        if((s[i] == ')' && bracArr[bracArr.length-1] == '(') || (s[i] == '}' && bracArr[bracArr.length-1] == '{') || (s[i] == ']' && bracArr[bracArr.length-1] == '[')){
+           bracArr.pop() 
+        }else{
+            bracArr.push(s[i])
+        }
+    }
+    if(bracArr.length != 0) return false
+    return true
+})('(])') //false
+
+//method 2
+
 function bracketMatcher(str)
 {
 	var tempArr = [];
