c31138fc67046eb90ef137c42be63783f5d6842a
Sohini Makar <sohini.makar@gmail.com>
2018-01-17 08:00:47 -0800

    input str can be segmented into a space-separated  sequence of dictionary words
    Given an input string and a dictionary of words, find out if the input string can be segmented into a space-separated 
    sequence of dictionary words

--- Algorithms/stringSegmentChecker.js
+++ Algorithms/stringSegmentChecker.js
@@ -34,3 +34,44 @@ console.log(stringSegmentChecker('iiiiiiii')) //Yes
 console.log(stringSegmentChecker('samsungandmangok')) //No
 console.log(stringSegmentChecker('ilikelikeimangoiii')) //Yes
 console.log(stringSegmentChecker('')) //Yes
+//--------------------------------------------------------------
+/*Given an input string and a dictionary of words, find out if the input string can be segmented into a space-separated 
+sequence of dictionary words. See following examples for more details.
+Consider the following dictionary 
+{ i, like, sam, sung, samsung, mobile, ice, 
+  cream, icecream, man, go, mango}
+Input:  ilike
+Output: [ 'i', 'like' ] 
+The string can be segmented as "i like".
+Input:  ilikesamsung
+Output: [ 'i', 'like', 'samsung' ]
+The string can be segmented as "i like samsung" 
+or "i like sam sung".
+*/
+
+function stringSegmentChecker(str){
+  var dictionary = ['i', 'like', 'sam', 'sung', 'samsung', 'mobile', 'ice', 
+  'cream', 'icecream', 'man', 'go', 'mango']
+  var temp = ''
+  var output = []
+  if(dictionary.includes(str)) return [str]
+  if(str.length == 0) return output
+  for(var i = 0; i < str.length; i++){
+    temp += str[i]
+    if(dictionary.includes(temp)){
+      output.push(temp)
+      return output.concat(stringSegmentChecker(str.slice(i+1)))
+    }
+    //alternate - start
+    /*if(dictionary.includes(temp)){
+      output.push(temp)
+      temp = ''
+    }*/
+    //alternate - end
+  }
+  return output
+}
+
+console.log(stringSegmentChecker('ilike')) //[ 'i', 'like' ]
+console.log(stringSegmentChecker('ilikesamsung')) //[ 'i', 'like', 'sam', 'sung' ]
+console.log(stringSegmentChecker('iiiiiiii')) //[ 'i', 'i', 'i', 'i', 'i', 'i', 'i', 'i' ]
