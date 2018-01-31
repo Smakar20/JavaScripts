bbda43e224d7fdf7581a302e56d46dc13adf7c78
Sohini Makar <sohini.makar@gmail.com>
2018-01-31 14:48:43 -0800

    binary search algorithm

--- Algorithms/BinarySearch.js
+++ Algorithms/BinarySearch.js
@@ -1,5 +1,21 @@
-
 //binary search algorithm
+
+function binarySearch(arr,num){
+  return binarySearchUtil(arr, num, 0, arr.length - 1)
+}
+
+function binarySearchUtil(arr, num, start, end){
+  if(arr.length == 0 || (arr.length == 1 && arr[0] != num) || (start > end)) return false
+  
+  var mid = parseInt((start+end)/2)
+  if(num == arr[mid]) return true
+  if(num < arr[mid]) return binarySearchUtil(arr,num,start,mid-1)
+  if(num > arr[mid]) return binarySearchUtil(arr,num,mid+1,end)
+}
+
+binarySearch([1,1,1,1,2,3,3,4,7],3) //true
+
+//alternate method
 function BinarySearch(arr,key)
 {
 	var midIdx = parseInt(arr.length/2);
