73b2403d8d70621ee495d2748f41e388295c10b2
Sohini Makar <sohini.makar@gmail.com>
2018-02-01 11:12:11 -0800

    reverse a String

--- Algorithms/reverseString.js
+++ Algorithms/reverseString.js
@@ -1,5 +1,41 @@
 //reverse a String
 
+function reverse(arr, sIdx, eIdx){
+  var j = 0
+  for(var i = sIdx; i <= Math.floor((sIdx + eIdx)/2); i++){
+    var temp = arr[i]
+    arr[i] = arr[eIdx - j]
+    arr[eIdx - j] = temp
+    j++
+  }
+  return arr
+}
+
+function solution(str){
+  var arr = reverse(str.split(''),0, str.length-1)
+  var stIdx = 0, letFound = false
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i] == ' ' && !letFound){
+      stIdx = i + 1
+      continue
+    }else{
+      letFound = true
+    }
+    
+    if(arr[i] == ' '){
+      reverse(arr,stIdx,i-1)
+      stIdx = i + 1
+    }
+  }
+  reverse(arr,stIdx,arr.length-1)
+  return arr.join('')
+}
+
+
+solution('Hello Sohi!!!') //'Sohi!!! Hello'
+
+
+//---------- alternate method ---------------
 function reverseString(str){
   var reversedStr = ''
   for(var i = str.length - 1; i >= 0; i--){
