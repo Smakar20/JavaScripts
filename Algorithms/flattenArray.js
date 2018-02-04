ee5d29e41613a524222f0004f3196e6c87a46555
Sohini Makar <sohini.makar@gmail.com>
2018-02-03 21:17:26 -0800

    flatten array

---
+++ Algorithms/flattenArray.js
@@ -0,0 +1,27 @@
+ /* If arr is not an array, it is just returned as-is.
+ 
+  Example
+     flatten(1)                     === 1
+     flatten([1, 2, 3])             === [1, 2, 3] 
+     flatten([[1], [2, 3]])         === [1, 2, 3] 
+     flatten([[[[1]]], [[[2]], 3]]) === [1, 2, 3]
+ */
+ 
+function flatten(arr) {
+  if(!(Array.isArray(arr))) return arr
+  var output = []
+  flattenUtil(arr,output)
+  return output
+}
+
+function flattenUtil(arr, output){
+  for(var i in arr){
+    if(Array.isArray(arr[i]) === false){
+      output.push(arr[i])
+    }else{
+      flattenUtil(arr[i], output)
+    }
+  }
+}
+
+flatten([[[[1]]], [[[2]], 3]]) // [1,2,3]
