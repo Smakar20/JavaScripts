4187180fc6a839ddd05be9da9c546dd39b89dded
Sohini Makar <sohini.makar@gmail.com>
2018-02-05 13:34:00 -0800

    Abs diagonal sum difference of a square matrix

--- Algorithms/AbsDiffMatrixSum.js
+++ Algorithms/AbsDiffMatrixSum.js
@@ -34,5 +34,5 @@ function MartixAbsSum(matrix)
 var matrix=[[5,2,3],
 			[3,4,5],
 			[5,6,7]];
-var calMatrixSum = new MartixAbsSum(matrix);
-console.log(calMatrixSum.matrixSum());
+var calMatrixSum = new MartixAbsSum(matrix)
+console.log(calMatrixSum.matrixSum())
