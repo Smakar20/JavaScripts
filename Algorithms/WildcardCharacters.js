3fe2011730b930e2bff805817657d5357870bbd9
Sohini Makar <sohini.makar@gmail.com>
2018-02-15 21:04:48 -0800

    wildcard characters

--- Algorithms/WildcardCharacters.js
+++ Algorithms/WildcardCharacters.js
@@ -1,6 +1,11 @@
-/*Have the function WildcardCharacters(str) read str which will contain two strings separated by a space. The first string will consist of the following sets of characters: +, *, and {N} which is optional. The plus (+) character represents a single alphabetic character, the asterisk (*) represents a sequence of the same character of length 3 unless it is followed by {N} which represents how many characters should appear in the sequence where N will be at least 1. Your goal is to determine if the second string exactly matches the pattern of the first string in the input. 
+/*Have the function WildcardCharacters(str) read str which will contain two strings separated by a space. The first string will consist of 
+the following sets of characters: +, *, and {N} which is optional. The plus (+) character represents a single alphabetic character, the 
+asterisk (*) represents a sequence of the same character of length 3 unless it is followed by {N} which represents how many characters 
+should appear in the sequence where N will be at least 1. Your goal is to determine if the second string exactly matches the pattern of the 
+first string in the input. 
 
-For example: if str is "++*{5} gheeeee" then the second string in this case does match the pattern, so your program should return the string true. If the second string does not match the pattern your program should return the string false.*/
+For example: if str is "++*{5} gheeeee" then the second string in this case does match the pattern, so your program should return the 
+string true. If the second string does not match the pattern your program should return the string false.*/
 
 function WildcardCharacters(str) { 
 
