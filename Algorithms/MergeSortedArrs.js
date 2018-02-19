b4114a7c87270cbbaa329623946cf7b9bbee1755
Sohini Makar <sohini.makar@gmail.com>
2018-02-19 10:58:00 -0800

    Update MergeSortedArrs.js

--- Algorithms/MergeSortedArrs.js
+++ Algorithms/MergeSortedArrs.js
@@ -1,4 +1,22 @@
-//Merging 2 sorted arrays.
+/*Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of 
+elements initialized in nums1 and nums2 are m and n respectively.*/
+
+var merge = function(nums1, m, nums2, n) {
+    var i = m-1
+    var j = n-1
+    for (var k = m+n-1; k >= 0; k--) {
+        if (i < 0) nums1[k] = nums2[j--]
+        else if (j < 0) nums1[k] = nums1[i--]
+        else if (nums1[i] < nums2[j]) nums1[k] = nums2[j--]
+        else nums1[k] = nums1[i--]
+    }
+};
+
+
+//--------Merging 2 sorted arrays----------
 'use strict'
 function sorting(arr1,arr2)
 {
