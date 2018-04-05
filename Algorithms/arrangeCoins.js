ebcbc597480708e93bd37a1c8716e20c7a8d435a
Sohini Makar <sohini.makar@gmail.com>
2018-04-05 14:16:58 -0700

    every k-th row must have exactly k coins

---
+++ Algorithms/arrangeCoins.js
@@ -0,0 +1,42 @@
+/*You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+
+Given n, find the total number of full staircase rows that can be formed.
+
+n is a non-negative integer and fits within the range of a 32-bit signed integer.
+
+Example 1:
+
+n = 5
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤
+
+Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+n = 8
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤ ¤
+¤ ¤
+
+Because the 4th row is incomplete, we return 3.
+ */
+
+var arrangeCoins = function(n) {
+    var i = n
+    var row = 1
+    while(i > 0){
+        i = i - row
+        if(i < 0) break
+        row++
+    }
+    return row-1
+};
+
+
+arrangeCoins(8) //3
