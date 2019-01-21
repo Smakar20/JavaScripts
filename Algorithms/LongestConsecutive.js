5434025fd13e5a823c0ce0f597587c0ef0c57d35
Sohini Makar <sohini.makar@gmail.com>
2019-01-20 23:20:34 -0800

    Update LongestConsecutive.js

--- Algorithms/LongestConsecutive.js
+++ Algorithms/LongestConsecutive.js
@@ -1,4 +1,8 @@
-/*Have the function LongestConsecutive(arr) take the array of positive integers stored in arr and return the length of the longest consecutive subsequence (LCS). An LCS is a subset of the original list where the numbers are in sorted order, from lowest to highest, and are in a consecutive, increasing order. The sequence does not need to be contiguous and there can be several different subsequences. For example: if arr is [4, 3, 8, 1, 2, 6, 100, 9] then a few consecutive sequences are [1, 2, 3, 4], and [8, 9]. For this input, your program should return 4 because that is the length of the longest consecutive subsequence.*/
+/*Have the function LongestConsecutive(arr) take the array of positive integers stored in arr and return the length of the longest 
+consecutive subsequence (LCS). An LCS is a subset of the original list where the numbers are in sorted order, from lowest to highest, 
+and are in a consecutive, increasing order. The sequence does not need to be contiguous and there can be several different subsequences. 
+For example: if arr is [4, 3, 8, 1, 2, 6, 100, 9] then a few consecutive sequences are [1, 2, 3, 4], and [8, 9]. For this input, 
+your program should return 4 because that is the length of the longest consecutive subsequence.*/
 
 (function LongestConsecutive(arr) { 
     arr = arr.sort((a,b)=>{return a-b})
