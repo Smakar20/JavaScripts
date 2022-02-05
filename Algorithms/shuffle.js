820ecb3a30b0a80eed641e23a991a0be69e2b82f
Sohini Makar <sohini.makar@gmail.com>
2022-02-04 22:35:45 -0800

    implement a shuffle() method

--- Algorithms/shuffle.js
+++ Algorithms/shuffle.js
@@ -1,20 +1,55 @@
-// Shuffle an Array
+/*
+How would you implement a shuffle() ?
 
-function getRandomInt(max) {
+When passed with an array, it should modify the array inline to generate a randomly picked permutation at the same probability.
+
+for an array like this:
+
+const arr = [1, 2, 3, 4]
+there would be possibly 4! = 24 permutations
+
+[1, 2, 3, 4]
+[1, 2, 4, 3]
+[1, 3, 2, 4]
+[1, 3, 4, 2]
+[1, 4, 2, 3]
+[1, 4, 3, 2]
+[2, 1, 3, 4]
+[2, 1, 4, 3]
+[2, 3, 1, 4]
+[2, 3, 4, 1]
+[2, 4, 1, 3]
+[2, 4, 3, 1]
+[3, 1, 2, 4]
+[3, 1, 4, 2]
+[3, 2, 1, 4]
+[3, 2, 4, 1]
+[3, 4, 1, 2]
+[3, 4, 2, 1]
+[4, 1, 2, 3]
+[4, 1, 3, 2]
+[4, 2, 1, 3]
+[4, 2, 3, 1]
+[4, 3, 1, 2]
+[4, 3, 2, 1]
+your shuffle() should transform the array in one of the above array, at the same 1/24 probability.
+
+notes: Your shuffle() will be called multiple times, to calculate the probability on each possible result, and test again standard deviation
+*/
+
+function shuffle(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const randonIdx = getRandomInx(i + 1);
+    [arr[i], arr[randonIdx]] = [arr[randonIdx], arr[i]];
+  }
+   // the output array here will have unpredictable order of elements. Meaning they are shuffled.
+  return arr;
+}
+
+function getRandomInx(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function Shuffle(arr) {
-  for ( var i=0; i < arr.length; ++i) {
-    var randomInt = getRandomInt(i)
-    // swap the two elements
-    var temp = arr[i]
-    arr[i] = arr[randomInt]
-    arr[randomInt] = temp
-  }
-  
-  // the output array here will have unpredictable order of elements. Meaning they are shuffled.
-  return arr
-}
-
-Shuffle([1,2,3,4,5,6])
+//--------- test -------------
+const arr = [1, 2, 3, 4];
+Shuffle(arr);
